d764aeb2dd1ce311839b6627649bc5523e1d99cd
NickDev90 <nickie05021990@gmail.com>
2020-11-16 22:33:29 +0100

    files added

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('.burger').click(function (argument) {
+		$('.burger,.menu').toggleClass('active'); 
+		$('body').toggleClass('lock');
+	});
+});
+
+
---
+++ learningOf.js
@@ -0,0 +1,34 @@
+// let browser;
+// if (browser='Edge'){
+// 	alert("You've got the Edge!");
+// }else if (browser='Chrome'||browser='Firefox'||browser='Safari'||browser='Opera') {
+// 	alert( 'Okay we support these browsers too' );
+// }else {
+// 	alert('We hope that this page looks ok!')
+// }
+
+// 'use strict'
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// switch (number){
+// 	case 0:
+// 	alert('Вы ввели число 0') ;
+// 	break;
+
+// 	case 1:
+// 	alert('Вы ввели число 1');
+// 	break;
+
+// 	case 2:
+// 	case 3:
+// 	alert('Вы ввели число 2, а может и 3')
+// }
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm('Родители разрешили?');
+  }
+}
+let age = prompt('');
