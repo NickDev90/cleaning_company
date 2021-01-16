1eae28d1f28b1ea648abce3657b19aa7b6ae0171
NickDev90 <nickie05021990@gmail.com>
2021-01-16 14:02:04 +0100

    bugs fixed

--- JS/main.js
+++ JS/main.js
@@ -3,5 +3,33 @@ $(function(){
 		$('.burger,.menu').toggleClass('active'); 
 		$('body').toggleClass('lock');
 	});
+	$('.menu').click(function(event) {
+		$('.burger,.menu').removeClass('active');
+		$('body').removeClass('lock');
+	});
 });
+
+
+// The same on native Javascript:
  
+// let liElements = document.querySelectorAll('.menu li');
+// let menu = document.querySelector('.menu');
+// let burger = document.querySelector('.burger');
+// let body = document.querySelector('body');
+
+// burger.onclick = () => {
+// 	burger.classList.toggle('active');
+// 	menu.classList.toggle('active');
+// 	body.classList.toggle('lock');
+// };
+
+// liElements.forEach(function (li) {
+// 	li.onclick = function () {
+// 		burger.classList.remove('active');
+// 		menu.classList.remove('active');
+// 		body.classList.remove('lock');
+// 	};
+// });
+
+
+	
